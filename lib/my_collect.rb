27bd12238b5b11212895b251400ae039ef4c6839
JC <bonassin@gmail.com>
2020-12-21 22:50:57 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
 def my_collect(array)
   counter = 0
   collection = []
-while i < array.length
-  collection << yield(array[counter])
-  counter += 1
-end
-collection
+  while i < array.length
+    collection << yield(array[counter])
+    counter += 1
+  end
+  collection
 end
